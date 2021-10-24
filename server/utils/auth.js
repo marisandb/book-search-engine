46cf7909d11df8b263fd8357bf871dfda2d488ad
maris <marisandb@yahoo.com>
2021-10-23 23:17:02 -0500

    updated auth and server files for GraphQL and Apollo

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -8,11 +8,14 @@ module.exports = {
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token
+      .split(' ')
+      .pop()
+      .trim();
     }
 
     if (!token) {

394ee52d53198525af685e1659870e4e9aa85e6f
maris <marisandb@yahoo.com>
2021-10-25 00:08:58 -0500

    fixing break

--- server/server.js
+++ server/server.js
@@ -6,17 +6,23 @@ const { typeDefs, resolvers } = require('./schemas');
 
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const server = new ApolloServer({ 
-  typeDefs, 
-  resolvers, 
-  context: authMiddleware 
-});
+const startServer = async () => {
+  const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers, 
+    context: authMiddleware 
+  });
+  
+await server.start();
+
 server.applyMiddleware({ app });
+
+startServer();
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());

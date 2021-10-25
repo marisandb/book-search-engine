65890922c2ecb43085a9e6c3fe4459fbcb2bfc93
maris <marisandb@yahoo.com>
2021-10-24 23:40:00 -0500

    added missing path for typdef and resolver

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
 const {authMiddleware} = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
+
 
 const path = require('path');
 const db = require('./config/connection');
@@ -10,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({ 
-  typeDef, 
+  typeDefs, 
   resolvers, 
   context: authMiddleware 
 });
